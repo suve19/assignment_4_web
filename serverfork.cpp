0a298b905aa94e221e316f4f9a6ffbf7b0fd43ed
sushma vendarapu <suve19@student.bth.se>
2024-10-17 19:36:53 +0000

    Initialize socket, bind to IP:PORT, and start listening
    Initializes the socket and adds all required libraries
    for the socket and bind and starts listening.

--- serverfork.cpp
+++ serverfork.cpp
@@ -1,24 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
-#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
-/* You will to add includes here */
+#define BACKLOG 10
 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <IP> <PORT>\n", argv[0]);
+        return 1;
+    }
 
+    const char *ip_address = argv[1];
+    int port = atoi(argv[2]);
 
-using namespace std;
+    int server_fd;
+    struct sockaddr_in server_addr;
 
-int main(int argc, char *argv[]){
-  
-  /* Do more magic */
+    // Create the socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
+    // Set up the server address struct
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip_address);
+    server_addr.sin_port = htons(port);
+    memset(&(server_addr.sin_zero), '\0', 8);
 
-  
-  printf("done.\n");
-  return(0);
+    // Bind the socket
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
+        perror("bind");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
+    // Start listening
+    if (listen(server_fd, BACKLOG) == -1) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
-  
+    printf("Server is running on %s:%d\n", ip_address, port);
+
+    close(server_fd);
+    return 0;
 }
