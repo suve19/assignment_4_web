7e9c28c511e789ce8eeafa31458dc6a0341fbe3e
sushma vendarapu <suve19@student.bth.se>
2024-10-19 19:21:17 +0000

    Initialize server: socket creation, binding, and listening

--- serverthread.cpp
+++ serverthread.cpp
@@ -1,24 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/time.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
-/* You will to add includes here */
+#define BACKLOG 10
 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <IP> <PORT>\n", argv[0]);
+        return 1;
+    }
 
+    const char *ip_address = argv[1];
+    int port = atoi(argv[2]);
+    int server_fd;
 
-using namespace std;
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;  // Support both IPv4 and IPv6
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-int main(int argc, char *argv[]){
-  
-  /* Do more magic */
+    // Resolve DNS and support both IPv4 and IPv6
+    if (getaddrinfo(ip_address, argv[2], &hints, &res) != 0) {
+        perror("getaddrinfo");
+        return 1;
+    }
 
+    // Create the server socket
+    if ((server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
+        perror("socket");
+        freeaddrinfo(res);
+        exit(EXIT_FAILURE);
+    }
 
-  
-  printf("done.\n");
-  return(0);
+    // Bind the socket
+    if (bind(server_fd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("bind");
+        close(server_fd);
+        freeaddrinfo(res);
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(res);
 
+    // Start listening for incoming connections
+    if (listen(server_fd, BACKLOG) == -1) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
-  
+    printf("Server is running on %s:%d\n", ip_address, port);
+
+    close(server_fd);
+    return 0;
 }
